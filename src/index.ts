1b087a57b230040343b8fc6d01ffdaaf41313ebe
兰鑫 <1192065030@qq.com>
2023-06-03 10:38:29 +0800

    refactor: 更改目录名字

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-// import './drop'
-// import './drop-draw'
-import './drop-draw-2'
+// import './01-drop'
+// import './02-drop-draw'
+import './03-drop-draw-2'
 
 if ((module as any).hot) {
   (module as any).hot.accept()
